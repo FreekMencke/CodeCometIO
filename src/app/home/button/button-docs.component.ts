cd0fad92eb06f0c4045e130d99ad6eb8f855e68c
Freek Mencke <freek_mencke@outlook.com>
2020-03-07 18:08:28 +0100

    added outline and flat button types

---
+++ src/app/home/button/button-docs.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'cc-button-docs',
+  templateUrl: './button-docs.component.html',
+  styleUrls: ['./button-docs.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ButtonDocsComponent { }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { MeteorButtonModule } from 'meteor';
 import { SharedModule } from 'src/shared/shared.module';
+import { ButtonDocsComponent } from './button/button-docs.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
+    ButtonDocsComponent,
   ],
   imports: [
     SharedModule,
