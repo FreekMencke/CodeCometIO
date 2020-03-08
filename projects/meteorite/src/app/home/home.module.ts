1f757b4d8a198df6c50f44b763a9e0bc3f229f41
Freek Mencke <freek_mencke@outlook.com>
2020-03-08 10:58:53 +0100

    restructure project

---
+++ projects/meteorite/src/app/home/home.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { MeteorButtonModule } from 'dist/meteor';
+import { SharedModule } from '../shared/shared.module';
+import { ButtonDocsComponent } from './button/button-docs.component';
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    ButtonDocsComponent,
+  ],
+  imports: [
+    SharedModule,
+    HomeRoutingModule,
+    MeteorButtonModule,
+  ],
+})
+export class HomeModule { }
