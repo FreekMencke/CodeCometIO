759c698c9815cce0768ba6bfcfbfa3ada42b2fc4
Freek Mencke <freek_mencke@outlook.com>
2020-06-25 17:55:56 +0200

    feat(components): improved mtr-header, added header and container docs

--- projects/meteorite/src/app/docs/docs.module.ts
+++ projects/meteorite/src/app/docs/docs.module.ts
@@ -14,6 +14,7 @@ import { ContainerDocsComponent } from './container/container-docs.component';
     DocsRoutingModule,
     MeteorButtonModule,
     MeteorSideMenuModule,
+    MeteorHeaderModule,
   ],
   declarations: [
     DocsComponent,
--- projects/meteorite/src/app/docs/header/header-docs.component.ts
+++ projects/meteorite/src/app/docs/header/header-docs.component.ts
@@ -3,6 +3,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'mit-header-docs',
   templateUrl: './header-docs.component.html',
+  styleUrls: ['./header-docs.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderDocsComponent { }
