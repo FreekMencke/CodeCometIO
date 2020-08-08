ff3dc2c82b8d9e8464527bc4693b71a1da629593
Freek Mencke <freek_mencke@outlook.com>
2020-08-08 13:48:15 +0200

    fix(mtr-header): fix mobile dropdown colors

--- projects/meteor/src/header/mtr-header.component.ts
+++ projects/meteor/src/header/mtr-header.component.ts
@@ -1,4 +1,7 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { fromEvent, Subject } from 'rxjs';
+import { filter, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'mtr-header',
@@ -7,31 +10,46 @@ import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnIni
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MeteorHeaderComponent implements OnInit, OnDestroy {
+  private readonly destroyed$: Subject<void> = new Subject();
+
   private breakpointDesktop: number;
 
   open: boolean = false;
 
-  constructor(private cdRef: ChangeDetectorRef) {}
+  constructor(private cdRef: ChangeDetectorRef, private router: Router) {}
 
   ngOnInit(): void {
     this.breakpointDesktop = Number(
       getComputedStyle(document.documentElement).getPropertyValue('--breakpoint-desktop').slice(0, -2),
     );
-    window.addEventListener('resize', this.onResize);
+
+    this.router.events
+      .pipe(
+        takeUntil(this.destroyed$),
+        filter(() => this.open),
+        filter(event => event instanceof NavigationEnd),
+      )
+      .subscribe(() => this.close());
+
+    fromEvent(window, 'resize')
+      .pipe(
+        takeUntil(this.destroyed$),
+        filter(() => this.open && this.breakpointDesktop <= window.innerWidth),
+      )
+      .subscribe(() => this.close());
   }
 
   ngOnDestroy(): void {
-    window.removeEventListener('resize', this.onResize);
+    this.destroyed$.next();
+    this.destroyed$.complete();
   }
 
   toggle(): void {
     this.open = !this.open;
   }
 
-  onResize = () => {
-    if (this.open && this.breakpointDesktop <= window.innerWidth) {
-      this.open = false;
-      this.cdRef.markForCheck();
-    }
+  close(): void {
+    this.open = false;
+    this.cdRef.markForCheck();
   }
 }
